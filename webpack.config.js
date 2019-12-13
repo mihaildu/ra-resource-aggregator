d1a7f63f145324705a9b1745c391395e721098af
Mihail Dunaev <dunaevmihail@yahoo.com>
2019-12-13 13:26:19 +0000

    add env & library to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,35 @@
 const path = require('path');
 const pkg = require('./package.json');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: 'main.js',
-    library: pkg.name,
-    libraryTarget: 'umd',
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
+module.exports = (env, argv) => {
+
+  const envToMode = env => {
+    if (env !== 'production' && env !== 'development') {
+      return 'none';
+    }
+    return env;
+  };
+
+  const envToFilename = env => {
+    if (env === 'production') {
+      return 'main.min.js';
+    }
+    return 'main.js';
+  };
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: envToFilename(env),
+      library: pkg.name,
+      libraryTarget: 'umd',
+    },
+    mode: envToMode(env),
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      ]
+    }
   }
 };
