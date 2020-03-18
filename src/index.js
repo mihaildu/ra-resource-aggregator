d0540a4d72ee012edfdefb0124714d84f2aa63d0
Gianni Moschini <gianni.moschini+github@gmail.com>
2020-03-18 18:28:46 +0000

    Pass sort on getAllRecords

--- src/index.js
+++ src/index.js
@@ -372,7 +372,7 @@ class DataProvider {
 
         if (queryType === 'GET_LIST') {
           queries.push({
-            query: this.getAllRecords({ resourceName, filter: newParams.filter }),
+            query: this.getAllRecords({ resourceName, filter: newParams.filter, sort: newParams.sort }),
             resourceName
           });
         } else if (queryType === 'GET_ONE') {
@@ -676,15 +676,15 @@ class DataProvider {
     }
   };
 
-  getAllRecords = ({ resourceName, filter = {} }) => {
+  getAllRecords = ({ resourceName, filter = {}, sort = { order: 'DESC' } }) => {
     return this.dataProvider('GET_LIST', resourceName, {
       pagination: { page: 1, perPage: 1 },
-      sort: { field: 'id', order: 'DESC' },
+      sort: sort,
       filter
     }).then(res => {
       return this.dataProvider('GET_LIST', resourceName, {
         pagination: { page: 1, perPage: res.total },
-        sort: { field: 'id', order: 'DESC' },
+        sort: sort,
         filter
       });
     });
